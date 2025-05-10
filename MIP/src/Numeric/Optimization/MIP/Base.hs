4c89db535d1c70322680fcb08c99125a642b60f7
Masahiro Sakai <masahiro.sakai@gmail.com>
2025-05-11 00:11:36 +0900

    improve haddock of FileOptions a little

--- MIP/src/Numeric/Optimization/MIP/Base.hs
+++ MIP/src/Numeric/Optimization/MIP/Base.hs
@@ -786,7 +786,7 @@ semiIntegerVariables mip = Map.keysSet $ Map.filter ((SemiIntegerVariable ==) . 
 data FileOptions
   = FileOptions
   { optFileEncoding :: Maybe TextEncoding
-    -- ^ Text encoding used for file input/output
+    -- ^ Text encoding used for file I/O
   , optMPSWriteObjSense :: WriteSetting
     -- ^ @OBJSENSE@ section in MPS file is an extention of MPS file
     -- format for specifying the direction of the objective function
@@ -795,15 +795,15 @@ data FileOptions
     --
     -- This option controls whether the @OBJSENSE@ sections is written.
     -- If 'WriteIfNotDefault' is used, @OBJSENSE@ is written when the
-    -- objective is maximization and @OBJSENSE@ is not written written
-    -- when the objective is minimizing.
+    -- objective is maximization and @OBJSENSE@ is not written
+    -- when the objective is minimization.
     --
     -- (Default: 'WriteIfNotDefault')
   , optMPSWriteObjName :: Bool
     -- ^ @OBJNAME@ section is an extention of MPS file format for
     -- selecting an objective function from among the free rows within
     -- a MPS file. Not all solver support it (e.g. GLPK-4.48
-    -- does not support @OBJNAME@ it).
+    -- does not support it).
     --
     -- This option controls whether the @OBJNAME@ section is written.
     --
